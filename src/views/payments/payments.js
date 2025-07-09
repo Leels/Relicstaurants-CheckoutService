6c81bea6a9c6aca73a1bd775c88ae212ac40f965
Lela Smith <smith.lelaa@gmail.com>
2025-07-08 20:40:22 -0700

    update API calls

--- src/views/payments/payments.js
+++ src/views/payments/payments.js
@@ -16,7 +16,7 @@ const Payments = () => {
   const { Option } = Select;
 
   const handleOrderSending = async (payload) => {
-    await axios.post('http://lvh.me:3002/api/checkout', payload);
+    await axios.post('http://3.133.81.34.me:3002/api/checkout', payload);
   };
 
   const columns = [
